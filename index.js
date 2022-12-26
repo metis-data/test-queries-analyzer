6af1d35e1c88e807189896817269effcc457147b
gideonshmila <gideons@metisdata.io>
2022-12-26 14:54:16 +0200

    M-porgess

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ try {
   const pullRequest = context.payload.pull_request;
   console.log(pullRequest.title);
   core.setOutput('pr_tag', pullRequest.title || 'Action not trigger from pr');
-
-  const runId = context.runId;
-  const { owner, repo } = context.repo;
-
-  // Get the status of the workflow run
-  const { data: run } = await github.actions.getWorkflowRun({
-    owner,
-    repo,
-    run_id: runId,
-  });
-
-  // Check if the workflow run has completed
-  if (run.conclusion === 'success' || run.conclusion === 'failure') {
-    console.log('Hellosdlfjlsdfjlsdjflsdjflsdjflsdfjlsdjflsdjflsdjfsdlfjsdlfjlsdfjsldjflsdjfsdfksdjflkdsjflsdjfsldkjf');
-  }
 } catch (error) {
   core.setFailed(error.message);
 }
