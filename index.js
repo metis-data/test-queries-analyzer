56080e38cd4ab31219653defe484ce73b643f1b3
gideonshmila <gideons@metisdata.io>
2022-12-27 11:00:28 +0200

    M-porgess

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Octokit } = require("@octokit/rest");
+const { context } = require('@actions/github')
+const octokit = github.getOctokit(core.getInput('github_token'));
 
 
-const commentOnPr = async () => {
-  const owner = process.env.GITHUB_REPOSITORY_OWNER;
-  const repo = process.env.GITHUB_REPOSITORY_NAME;
-    // Create the comment on the pull request
-    const octokit = new Octokit({
-      auth: core.getInput('github_token'),
-    }); 
-    await octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number: pullRequest.number,
-      body: "This is a new comment on the pull request",
+async function commentPr() {
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! We will'
   });
 }
+
+
+
 
 try {
   const context = github.context;
@@ -24,7 +21,7 @@ try {
   const pullRequest = context.payload.pull_request;
   console.log(pullRequest.title);
   core.setOutput('pr_tag', pullRequest.title || 'Action not trigger from pr');
-  commentOnPr();
+  commentPr();
 
 
 } catch (error) {
