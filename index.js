008efe736750cdc8a862d5f9ccdd30c7db819b7b
gideonshmila <gideons@metisdata.io>
2023-01-01 17:18:10 +0200

    M-porgess

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { context } = require('@actions/github')
+const { context } = require('@actions/github');
 const octokit = github.getOctokit(core.getInput('github_token'));
 
 const { pull_request } = context.payload;
 
-async function commentPr() {
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: `https://app.metisdata.io/tests/${core.getInput('metis_api_key')}`
-  });
-}
+const commentPr = async () => {
+  try {
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: `${core.getInput('target_url')}/tests/${core.getInput('metis_api_key')}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-
+const createNewTest = async () => {
+  try {
+    axios
+      .post(`${core.getInput('target_url')}/tests/${core.getInput('metis_api_key')}`, {
+        name: pull_request.title,
+        apiKey: core.getInput('metis_api_key'),
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 try {
   const context = github.context;
@@ -22,9 +41,8 @@ try {
   const pullRequest = context.payload.pull_request;
   console.log(pullRequest.title);
   core.setOutput('pr_tag', pullRequest.title || 'Action not trigger from pr');
+  createNewTest();
   commentPr();
-
-
 } catch (error) {
   core.setFailed(error.message);
 }
