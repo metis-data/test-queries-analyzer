f2cd97b8025e15b5c157074220aefe176f06e96a
gideonshmila <gideons@metisdata.io>
2023-01-04 15:01:07 +0200

    M-porgess

---
+++ post.js
@@ -0,0 +1,37 @@
+import * as core from '@actions/core'
+import axios from 'axios'
+const {context} = require('@actions/github')
+
+const {pull_request} = context.payload
+
+const updateTestIsCompleted = async () => {
+  try {
+    const urlPrefix = core.getInput('target_url') || `https://ingest-stg.metisdata.io`;
+    axios
+      .post(
+        `${urlPrefix}/api/tests/update-test-to-completed`,
+        {
+          name: pull_request.title,
+          apiKey: core.getInput('metis_api_key')
+        }
+      )
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const run = async () => {
+  try {
+    await updateTestIsCompleted()
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run();
