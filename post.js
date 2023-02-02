25f32015047efde859b5d8fb0252a3d76ea35b56
gideonshmila <gideons@metisdata.io>
2023-02-02 11:21:20 +0200

    change to correct url in post.js

--- post.js
+++ post.js
@@ -6,7 +6,7 @@ const {pull_request} = context.payload
 
 const updateTestIsCompleted = async () => {
   try {
-    const urlPrefix = core.getInput('target_url') || `https://ingest.metisdata.io`;
+    const urlPrefix = core.getInput('target_url') || `https://app.metisdata.io`;
     axios
       .post(
         `${urlPrefix}/api/tests/update-test-to-completed`,
